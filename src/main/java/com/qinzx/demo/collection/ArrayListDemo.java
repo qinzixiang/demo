257625d4b8456813996bc56c218bcc8a7c7bcae6
qinzx <qinzx@bestclick.tech>
2019-12-13 16:46:38 +0800

    增加集合操作示例

--- src/main/java/com/qinzx/demo/collection/ArrayListDemo.java
+++ src/main/java/com/qinzx/demo/collection/ArrayListDemo.java
@@ -3,6 +3,7 @@ package com.qinzx.demo.collection;
 import cn.hutool.core.lang.Assert;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 测试list
@@ -21,6 +22,25 @@ public class ArrayListDemo {
         strings.add(null);
         Assert.isTrue(strings.contains(""));
         Assert.isTrue(strings.contains(null));
+        ArrayList<Long> orgIds = new ArrayList<>();
+        orgIds.add(11L);
+        orgIds.add(121L);
+        orgIds.add(56L);
+        orgIds.add(121L);
+        orgIds.add(19L);
+        System.out.println(orgIds);
+        List<Long> newOrgIds = new ArrayList<>();
+        newOrgIds.add(121L);
+        newOrgIds.add(11L);
+        newOrgIds.add(121L);
+        //取交集不去重，结果中会出现重复的元素
+        orgIds.retainAll(newOrgIds);
+        System.out.println(orgIds);
+        //只删除匹配的第一个元素
+//        orgIds.remove(121L);
+        //java8 新增的，删除使括号内的函数为true的元素
+        orgIds.removeIf(Long.valueOf(121)::equals);
 
+        System.out.println(orgIds);
     }
 }
