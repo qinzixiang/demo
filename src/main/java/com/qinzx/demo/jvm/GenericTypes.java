a825fa9717d8ff1b786a66c64bb70e7f93364207
qinzx <qinzxiang@163.com>
2020-03-25 18:56:24 +0800

    泛型重载示例

---
+++ src/main/java/com/qinzx/demo/jvm/GenericTypes.java
@@ -0,0 +1,26 @@
+package com.qinzx.demo.jvm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 泛型重载示例
+ * @author qinzx
+ * @date 2020/03/25 16:55
+ * @Copyright (C) 杭州同基汽车科技有限公司
+ */
+public class GenericTypes {
+    public static String method(List<String> list) {
+        System.out.println("invoke   method(List<String> list)");
+        return "";
+    }
+
+    public static int method(List<Integer> list) {
+        System.out.println("invoke   method(List<Integer> list)");
+        return 1;
+    }
+    public static void main(String[] args){
+        method(new ArrayList<String>());
+        method(new ArrayList<Integer>());
+    }
+}
